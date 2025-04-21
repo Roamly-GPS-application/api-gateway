caafe55c994c070c476ca93144c307321ea17f4a
Nikol <n.genova@student.fontys.nl>
2025-04-21 11:53:34 +0300

    Fixed missing dependencies in pom file as well as added additional route in gateway config

--- src/main/java/Roamly/API/Gateway/config/GatewayConfig.java
+++ src/main/java/Roamly/API/Gateway/config/GatewayConfig.java
@@ -11,8 +11,12 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(r -> r.path("/api/{segment}")
-                        .filters(f -> f.rewritePath("/api/(?<segment>.*)", "/${segment}"))
+                .route(r -> r.path("/api/poi/**")
+                        .filters(f -> f.rewritePath("/api/poi/(?<segment>.*)", "/poi/${segment}"))
+                        .uri("http://localhost:8001"))
+
+                .route(r -> r.path("/api/location/**")
+                        .filters(f -> f.rewritePath("/api/location/(?<segment>.*)", "/location/${segment}"))
                         .uri("http://localhost:8001"))
 
                 .build();
