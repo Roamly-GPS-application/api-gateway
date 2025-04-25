668601919c855baca608cb500ef6fc659340101c
Nikol <n.genova@student.fontys.nl>
2025-04-25 16:02:08 +0300

    adding an internal token for security access to the services

--- src/main/java/Roamly/API/Gateway/config/GatewayConfig.java
+++ src/main/java/Roamly/API/Gateway/config/GatewayConfig.java
@@ -1,23 +1,59 @@
 package Roamly.API.Gateway.config;
 
+import Roamly.API.Gateway.ApiGatewayApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.parameters.P;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 public class GatewayConfig {
 
+    private final static Logger logger = LoggerFactory.getLogger(ApiGatewayApplication.class);
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+
+        Properties prop = new Properties();
+        String internalToken = null;
+
+       try (InputStream input = getClass().getClassLoader().getResourceAsStream("eureka.properties")){
+
+           if(input == null){
+               System.out.println("Couldn't find properties file");
+           }
+
+           prop.load(input);
+
+           internalToken = prop.getProperty("INTERNAL_TOKEN");
+
+       }catch(Exception ex){
+           System.out.print(ex);
+       }
+
+       String token = internalToken;
+
+
+        logger.debug("Internal token {}", token);
+
+
         return builder.routes()
 
                 .route(r -> r.path("/api/location/**")
-                        .filters(f -> f.rewritePath("/api/location/(?<segment>.*)", "/location/${segment}"))
+                        .filters(f -> f
+                                .addRequestHeader("X-Internal-Token", token)
+                                .rewritePath("/api/location/(?<segment>.*)", "/location/${segment}"))
                         .uri("lb://LOCATION-AND-POI-SERVICE"))
 
                 .route(r -> r.path("/api/poi/**")
-                        .filters(f -> f.rewritePath("/api/poi/(?<segment>.*)", "/poi/${segment}"))
+                        .filters(f -> f
+                                .addRequestHeader("X-Internal-Token", token)
+                                .rewritePath("/api/poi/(?<segment>.*)", "/poi/${segment}"))
                         .uri("lb://LOCATION-AND-POI-SERVICE"))
 
                 .build();
