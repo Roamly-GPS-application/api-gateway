c5c732f2a28e2d0f22413e4528b9355f7e3a1f22
Nikol <n.genova@student.fontys.nl>
2025-06-11 10:26:55 +0200

    redeploy 3

--- src/main/java/Roamly/API/Gateway/ApiGatewayApplication.java
+++ src/main/java/Roamly/API/Gateway/ApiGatewayApplication.java
@@ -2,11 +2,8 @@ package Roamly.API.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
