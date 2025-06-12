9818391e4187b03f5a6dfa3cac05503915b3dcdb
Nikol <n.genova@student.fontys.nl>
2025-06-12 18:58:11 +0200

    added custom security config for the load-testing header

--- src/main/java/Roamly/API/Gateway/config/SecurityConfig.java
+++ src/main/java/Roamly/API/Gateway/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package Roamly.API.Gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -17,17 +19,32 @@ import java.util.List;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private static final List<String> ALLOWED_HEADERS = List.of("Authorization", "Content-type");
+    private static final List<String> ALLOWED_HEADERS = List.of("Authorization", "Content-type", "Load-testing");
     private static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
     private static final List<String> ALLOWED_ORIGINS = List.of("http://localhost:5173", "https://100.64.195.48", "http://100.64.195.48", "https://174.138.105.226.nip.io");
 
+    @Value("${LOAD_TESTS_TOKEN}")
+    private String loadTestingToken;
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        ServerWebExchangeMatcher headerMatcher = exchange -> {
+            String testToken = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst("Load-testing");
+
+            boolean ok = (testToken != null && loadTestingToken.equals(testToken));
+            return ok
+                    ? ServerWebExchangeMatcher.MatchResult.match()
+                    : ServerWebExchangeMatcher.MatchResult.notMatch();
+
+        };
 
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
+                        .matchers(headerMatcher).permitAll()
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .pathMatchers("/metrics").permitAll()
                         .pathMatchers("/api/**").authenticated()
